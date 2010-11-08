e46cb10412d51af4e24245a763fba2fdc8b5df23
rpruim (Randall Pruim) <rpruim@calvin.edu>
2010-11-08 18:08:47 +0000

    Added rgolfballs and deathPen data sets moved show.chisq.test() to xchisq.test()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fastr@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/fastR/R/xqqmath.R
+++ pkg/fastR/R/xqqmath.R
@@ -1,9 +1,23 @@
 xqqmath <-
-function (x, ..., lcol = trellis.par.get("add.line")$col) 
+function (x, qqmathline = FALSE, idline = FALSE, fitline = FALSE, 
+    slope = sd(x), intercept = mean(x), ..., lcol = trellis.par.get("add.line")$col, 
+    pch = 16, lwd = 2, lty = 2) 
 {
-    qqmath(x, prepanel = prepanel.qqmathline, panel = function(x, 
-        ...) {
-        panel.qqmath(x, ...)
-        panel.qqmathline(x, col = lcol, ...)
-    }, ...)
+    lty <- rep(lty, length = 3)
+    qqmath(x, prepanel = prepanel.qqmathline, pch = pch, xlab = "theoretical quantiles", 
+        ylab = "empirical quantiles", panel = function(x, ...) {
+            panel.qqmath(x, ...)
+            if (idline) {
+                panel.abline(0, 1, col = lcol, lty = lty[3], 
+                  lwd = lwd)
+            }
+            if (fitline) {
+                panel.abline(intercept, slope, col = lcol, lty = lty[2], 
+                  lwd = lwd)
+            }
+            if (qqmathline) {
+                panel.qqmathline(x, col = lcol, lty = lty[1], 
+                  lwd = lwd, ...)
+            }
+        }, ...)
 }
