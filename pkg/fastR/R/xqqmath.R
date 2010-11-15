d1ea007f496ed1747f3298ed4ac24f7d1a6349ab
rpruim (Randall Pruim) <rpruim@calvin.edu>
2010-11-15 00:03:42 +0000

    Modified xqqmath() to handle both formulas and numeric vectors better. Added overlines feature to xqqmath. Documented xqqmath() Removed from pheno subjects who are offspring of other subjects. Removed subjects from fusion1 and fusion2 who did not appear in pheno. Cleaned up syntax error in favstats.Rd
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fastr@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/fastR/R/xqqmath.R
+++ pkg/fastR/R/xqqmath.R
@@ -1,23 +1,5 @@
 xqqmath <-
-function (x, qqmathline = FALSE, idline = FALSE, fitline = FALSE, 
-    slope = sd(x), intercept = mean(x), ..., lcol = trellis.par.get("add.line")$col, 
-    pch = 16, lwd = 2, lty = 2) 
+function (x, ...) 
 {
-    lty <- rep(lty, length = 3)
-    qqmath(x, prepanel = prepanel.qqmathline, pch = pch, xlab = "theoretical quantiles", 
-        ylab = "empirical quantiles", panel = function(x, ...) {
-            panel.qqmath(x, ...)
-            if (idline) {
-                panel.abline(0, 1, col = lcol, lty = lty[3], 
-                  lwd = lwd)
-            }
-            if (fitline) {
-                panel.abline(intercept, slope, col = lcol, lty = lty[2], 
-                  lwd = lwd)
-            }
-            if (qqmathline) {
-                panel.qqmathline(x, col = lcol, lty = lty[1], 
-                  lwd = lwd, ...)
-            }
-        }, ...)
+    UseMethod("xqqmath", x)
 }
